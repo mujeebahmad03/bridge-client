8bee094cd0c32bdb9093701d622d379a5ffb29be
Abdulmujeeb Ahmad <abdulmujeebahmad03@gmail.com>
2025-10-28 05:15:18 +0100

    chore: update dependencies and add new components
    - Added several new dependencies including Radix UI components and date-fns.
    - Introduced new form field components: CheckboxFormField, DateFormField, and InputFormField.
    - Created utility functions for class name management.
    - Added a new components.json configuration file for UI settings.
    - Updated global styles and TypeScript configuration for improved path aliases.

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,15 +1,10 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
+import { Inter } from "next/font/google";
 
 import "@/styles/globals.css";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
+const inter = Inter({
+  variable: "--font-inter",
   subsets: ["latin"],
 });
 
@@ -24,12 +19,8 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
-        {children}
-      </body>
+    <html lang="en" suppressHydrationWarning>
+      <body className={`${inter.variable} antialiased`}>{children}</body>
     </html>
   );
 }
