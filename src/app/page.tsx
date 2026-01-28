0254c749b038b3f5be372b1c7415f8a9217e5557
Abdulmujeeb Ahmad <abdulmujeebahmad03@gmail.com>
2026-01-28 11:35:45 +0100

    feat: redirect to dashboard on home page load
    - Implemented a redirect to the dashboard when the home page is accessed.
    - This change enhances user navigation by directing users to the main application area immediately.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,10 @@
+import { redirect } from "next/navigation";
+
 import { Logo } from "@/components/common";
 
 export default function Home() {
+  redirect("/dashboard");
+
   return (
     <>
       <Logo />
